46012a129ba9b5a4cc12fc96935571a0054f2306
Carl Zhang <zhang.carl.x@gmail.com>
2023-12-28 12:49:18 -0500

    start today

--- web/app/components/focusedtask.component.tsx
+++ web/app/components/focusedtask.component.tsx
@@ -49,9 +49,9 @@ export default function FocusedTask() {
           <p className="text-xl text-neutral-800">{focusedTask['notes']}</p>
           <button className="bg-neutral-50 border-2 border-neutral-800 text-neutral-800 p-3 text-xl hover:bg-neutral-800 hover:text-neutral-100 font-bold" onClick={() => {finish();unfocus();}}>finish</button>
         </div>
-        <button className="absolute right-4 top-2" onClick={unfocus}>X</button>
+        <button className="absolute right-4 top-2 hover:text-neutral-400" onClick={unfocus}>X</button>
       </div>
-      <div className="bg-neutral-200 opacity-90 w-full h-full absolute -z-50" />
+      <div className="bg-neutral-200 opacity-80 w-full h-full absolute -z-50" />
     </div>
   )
 }
